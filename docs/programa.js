e686cb93f96b13a9caf19cac94b0bb24b67aac65
Paulo César Coronado <paulocoronado@udistrital.edu.co>
2025-04-30 15:23:31 -0500

    v 0.5

---
+++ docs/programa.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.444994369975801, -74.0467043888626], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
